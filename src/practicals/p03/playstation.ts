fd7b4abecd82fe4e36342a155b718cd96b159285
sirisart <mr.sirisart1999@gmail.com>
2026-02-16 10:10:16 +0700

    10

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,19 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+    gen: number;
+
+    constructor(name: string, gen: number, price: number) {
+        super(name);
+        this.gen = gen;
+        this.setPrice(price); 
+    }
+    getProfile():string{
+      return `${this.name} (Gen ${this.gen})`;
+    }
+    getDiscountPrice():number{
+      // 17091 (ถ้า Product.DISCOUNT_PERCENT = 10, คำนวณจาก 18990 - (18990 * 10 / 100))
+      const discount = this.price - (this.price *10 /100)
+      return discount;
+    }
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,14 +1,15 @@
 export class Product {
     name?:string;
-    private _price?:number = 0;
+    protected price:number = 0;
     constructor(name:string){
         this.name = name;
     }
     getPrice():number | undefined{
-        return this._price;
+        return this.price;
     }
     setPrice(amount:number){
-        this._price = amount;
+        this.price = amount;
+
     }
 }
 
