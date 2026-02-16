ba32de13e375e0d9fa5185c4425392a301adf152
sirisart <mr.sirisart1999@gmail.com>
2026-02-16 09:26:27 +0700

    4

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -3,7 +3,6 @@ export class Student {
     lastname?:string;
 
     printName():string{
-        console.log(this.firstname," ",this.lastname);
-        return "";
+        return this.firstname + " " + this.lastname;
     }
 }
