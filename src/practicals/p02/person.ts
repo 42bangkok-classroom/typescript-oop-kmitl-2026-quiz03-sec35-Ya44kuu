628bd51fe3c5c3a41111cf9bd957e3fd820a72f4
sirisart <mr.sirisart1999@gmail.com>
2026-02-16 09:38:18 +0700

    5

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,11 +1,16 @@
 export class Person {
     firstname?:string;
     lastname?:string;
+    age?:number;
+    static COUNTRY: string = "Thailand";
+    setAge(age:number){
+        this.age = age
+    }
 
     getFullName():string{
         return `${this.firstname} ${this.lastname}`;
     }
-    getAge():number{
-
+    getAge():number | undefined{
+        return this.age;
     }
 }
